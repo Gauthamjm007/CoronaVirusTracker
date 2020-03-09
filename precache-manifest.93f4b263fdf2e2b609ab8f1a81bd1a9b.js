1b068a85bf7a8a7a7b23f1eb4b202e4a49fb4853
Gauthamjm007 <j.mgautham15@gmail.com>
2020-03-09 23:26:07 +0530

    Updates

---
+++ precache-manifest.93f4b263fdf2e2b609ab8f1a81bd1a9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b5684fb74709694fa3b38b10aa0e50",
+    "url": "/CoronaVirusTracker/index.html"
+  },
+  {
+    "revision": "c3c2484a8fc81183388b",
+    "url": "/CoronaVirusTracker/static/css/main.fc5d01f0.chunk.css"
+  },
+  {
+    "revision": "6d19e983a208b3dd97ab",
+    "url": "/CoronaVirusTracker/static/js/2.faaa64ed.chunk.js"
+  },
+  {
+    "revision": "ffaab742c933bb1945e629800342db03",
+    "url": "/CoronaVirusTracker/static/js/2.faaa64ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3c2484a8fc81183388b",
+    "url": "/CoronaVirusTracker/static/js/main.97f86f47.chunk.js"
+  },
+  {
+    "revision": "ada5364577f148b856e1",
+    "url": "/CoronaVirusTracker/static/js/runtime-main.25d017f9.js"
+  }
+]);
